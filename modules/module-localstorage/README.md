1a245df5627c8db6ca10231f4e4428f9e1c02eda
Fahad Heylaal <fahad19@gmail.com>
2025-09-29 23:01:58 +0200

    localstorage module

---
+++ modules/module-localstorage/README.md
@@ -0,0 +1,9 @@
+# @eventvisor/module-localstorage
+
+> LocalStorage module for Eventvisor, for both persistence layer and lookups.
+
+Visit [https://eventvisor.org/docs/modules/localstorage/](https://eventvisor.org/docs/modules/localstorage/) for more information.
+
+## License
+
+MIT © [Fahad Heylaal](https://fahad19.com)
---
+++ modules/module-localstorage/src/index.ts
@@ -0,0 +1,59 @@
+import { Module } from "@eventvisor/sdk";
+
+export type LocalStorageModuleOptions = {
+  name?: string;
+  prefix?: string;
+};
+
+export function createLocalStorageModule(options: LocalStorageModuleOptions = {}): Module {
+  const { prefix = "_eventvisor_", name = "localstorage" } = options;
+
+  return {
+    name,
+
+    // this is different than storage use cases,
+    // and prefix is not meant to be used here
+    lookup: async ({ key }) => {
+      return window.localStorage.getItem(key);
+    },
+
+    readFromStorage: async ({ key }) => {
+      const fullKey = `${prefix}${key}`;
+
+      const value = window.localStorage.getItem(fullKey);
+
+      if (!value) {
+        return null;
+      }
+
+      try {
+        const parsedValue = JSON.parse(value);
+
+        if (parsedValue && typeof parsedValue.data !== "undefined") {
+          return parsedValue.data;
+        }
+        // eslint-disable-next-line
+      } catch (e) {
+        return null;
+      }
+
+      return null;
+    },
+
+    writeToStorage: async ({ key, value }) => {
+      const fullKey = `${prefix}${key}`;
+
+      const stringifiedValue = JSON.stringify({
+        data: value,
+      });
+
+      window.localStorage.setItem(fullKey, stringifiedValue);
+    },
+
+    removeFromStorage: async ({ key }) => {
+      const fullKey = `${prefix}${key}`;
+
+      window.localStorage.removeItem(fullKey);
+    },
+  };
+}
