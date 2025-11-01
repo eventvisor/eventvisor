c4995d64261a90cbb66f4290b8fcc0042c1d3ea8
Fahad Heylaal <fahad19@gmail.com>
2025-11-01 19:21:36 +0100

    change option name

--- modules/module-datadog-browser/src/index.ts
+++ modules/module-datadog-browser/src/index.ts
@@ -2,20 +2,20 @@ import type { Module } from "@eventvisor/sdk";
 
 export type DatadogBrowserModuleOptions = {
   name?: string;
-  rum: any; // @TODO: type this later
+  datadogRum: any; // @TODO: type this later
 };
 
 export function createDatadogBrowserModule(options: DatadogBrowserModuleOptions): Module {
-  const { name = "datadog-browser", rum } = options;
+  const { name = "datadog-browser", datadogRum } = options;
 
   return {
     name,
 
     transport: async ({ eventName, payload, error }) => {
       if (error) {
-        rum.addError(error, payload);
+        datadogRum.addError(error, payload);
       } else {
-        rum.addAction(eventName, payload);
+        datadogRum.addAction(eventName, payload);
       }
     },
   };
